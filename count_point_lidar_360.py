3e370570a3afc7c65a8fc8b381c7e2c4d8b3dc63
Tưởng Đăng Vương Quốc <64119031+cuoicungtui@users.noreply.github.com>
2023-12-27 17:25:58 +0700

    Create count_point_lidar_360.py

---
+++ count_point_lidar_360.py
@@ -0,0 +1,69 @@
+import serial
+import time
+from CalcLidarData import CalcLidarData
+
+ser = serial.Serial(port='/dev/tty.usbserial-0001',
+                    baudrate=230400,
+                    timeout=5.0,
+                    bytesize=8,
+                    parity='N',
+                    stopbits=1)
+
+tmpString = ""
+points_in_one_revolution = 0
+start_time = time.time()
+revolutions = 0
+
+while True:
+    loopFlag = True
+    flag2c = False
+
+    while loopFlag:
+        b = ser.read()
+        tmpInt = int.from_bytes(b, 'big')
+
+        if tmpInt == 0x54:
+            tmpString += b.hex() + " "
+            flag2c = True
+            continue
+
+        elif tmpInt == 0x2c and flag2c:
+            tmpString += b.hex()
+
+            if not len(tmpString[0:-5].replace(' ', '')) == 90:
+                tmpString = ""
+                loopFlag = False
+                flag2c = False
+                continue
+
+            lidarData = CalcLidarData(tmpString[0:-5])
+
+            # Increment the points counter for each point in the lidar data
+            points_in_one_revolution += len(lidarData.Angle_i)
+
+            tmpString = ""
+            loopFlag = False
+
+        else:
+            tmpString += b.hex() + " "
+
+        flag2c = False
+
+    if 'lidarData' in locals():
+        # Check if one revolution is complete (360 degrees)
+        if sum(lidarData.Angle_i) >= 360.0:
+            revolutions += 1
+            print(f"Revolution {revolutions}: Points in one revolution: {points_in_one_revolution}")
+
+            # Reset points counter for the next revolution
+            points_in_one_revolution = 0
+
+    elapsed_time = time.time() - start_time
+
+    # Check if 1 second has passed
+    if elapsed_time >= 1.0:
+        print(f"Processing frequency: {revolutions} revolutions per second")
+
+        # Reset counters and timer
+        revolutions = 0
+        start_time = time.time()
